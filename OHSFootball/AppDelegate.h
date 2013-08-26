71c602f8577901d4aaaef794983f88ffdd439901
Sam Gutentag <samgutentag@me.com>
2013-08-26 18:11:37 -0400

    Initial Commit

---
+++ OHSFootball/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OHSFootball
+//
+//  Created by Sam Gutentag on 8/26/13.
+//  Copyright (c) 2013 Sam Gutentag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OHSFootball/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OHSFootball
+//
+//  Created by Sam Gutentag on 8/26/13.
+//  Copyright (c) 2013 Sam Gutentag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
